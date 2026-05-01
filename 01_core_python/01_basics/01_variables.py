00f270081a7bd3a2d0045c8e642a6178d17b8349
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-05-01 23:56:11 +0530

    variables overview and proper folder setup

---
+++ 01_core_python/01_basics/01_variables.py
@@ -0,0 +1,43 @@
+# python-core-mastery/
+# print("Hello World")
+# print("My name is Nilesh")
+# print("Hi, I have started exploring python from today")
+# print('''we'll soon be comfortable in python, if we keep practicing''')
+
+
+# print("Shradha is my name.")
+# print("My age is 23.")
+# print("Shradha is my name", "My age is 23")
+
+# print(23)
+# print(23 + 35)
+
+# name = "shradha"
+# age = 23
+# age2 = age
+# price = 25.99
+
+# print("name")
+# print(name)
+# print(age)
+# print(price)
+# print("my name is:", name)
+# print("my age is:", age)
+# print(age2)
+
+# name = "Shradha"
+# age = 25
+# price = 25.99
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+
+# name1 = "sk"
+# name2 = 'sk'
+# name3 = '''sk'''
+
+# print(name1)
+# print(name2)
+# print(name3)
